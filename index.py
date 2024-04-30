c441355708ea188d31cf07a26866769957130a61
Yasiru Lakshan <59885914+yasirulakshan@users.noreply.github.com>
2024-04-30 23:59:12 +0530

    Add send_notification route to index.py

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +8,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+
+@app.route('/send_notification', methods=['POST'])
+def send_notification():
+    data = request.get_json()
+    data['appId'] = 21063
+    data['appToken'] = "Ddk9268wY7cmcRKQEcY5zI"
+    data['dateSent'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    data['title'] = "Intruder Detected"
+
+    url = "https://app.nativenotify.com/api/notification"
+
+    requests.post(url, json=data)
+
+    # Send notification to user
+
+    return "Done"
 
 
 if __name__ == '__main__':
