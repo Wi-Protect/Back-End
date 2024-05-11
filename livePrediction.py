1c07019eb6f79e100c8d108bc5d26354fe054091
Yasiru Lakshan <59885914+yasirulakshan@users.noreply.github.com>
2024-05-11 20:03:09 +0530

    Refactor livePrediction.py for improved data handling and recent variable initialization

--- livePrediction.py
+++ livePrediction.py
@@ -72,8 +72,6 @@ def csi_data_read_parse():
 
     startTime = datetime.datetime.now()
 
-    oneDeleted = False
-
     while True:
         strings1 = str(ser1.readline())
         strings2 = str(ser2.readline())
@@ -165,9 +163,8 @@ def csi_data_read_parse():
             timeString = now.strftime("%H:%M:%S.%f")
             csi_list = np.vstack(
                 [csi_list, [csi_data1[1]] + [timeString] + amplitudes+phases + [csi_data1[3]] + [csi_data2[3]]])
-            if count == 1 and not oneDeleted:
+            if count == 1:
                 csi_list = np.delete(csi_list, np.s_[:1], axis=0)
-                oneDeleted = True
 
         else:
 
@@ -186,7 +183,7 @@ def csi_data_read_parse():
                 else:
                     print("csi_list is empty")
 
-                    count = 0
+                    count = 1
 
                     csi_list = np.zeros((1, 260))
 
