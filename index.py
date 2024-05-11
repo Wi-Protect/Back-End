17f5cddeab3e4d20d5f76b5d6f386d619380643d
Yasiru Lakshan <59885914+yasirulakshan@users.noreply.github.com>
2024-05-11 16:07:33 +0530

    Refactor routes to use correct HTTP methods

--- index.py
+++ index.py
@@ -10,26 +10,26 @@ def hello_world():
     return 'Hello World'
 
 
-@app.route('/start_prediction', ['POST'])
+@app.route('/start_prediction', methods=['POST'])
 def predict():
 
     startPredictions()
     return "Prediction started"
 
 
-@app.route('/stop_prediction', ['POST'])
+@app.route('/stop_prediction', methods=['POST'])
 def stop_prediction():
     stopPredictions()
     return "Prediction stopped"
 
 
-@app.route('/night-mode', methods = ['POST'])
+@app.route('/night-mode', methods=['POST'])
 def night_mode():
     changeModeToNight()
     return "Night mode activated"
 
 
-@app.route('/day-mode', methods = ['POST'])
+@app.route('/day-mode', methods=['POST'])
 def day_mode():
     changeModeToNight()
     return "Day mode activated"
