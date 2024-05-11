ef141466d3e766891b57fafda5b4e0e8ff398a6f
Yasiru Lakshan <59885914+yasirulakshan@users.noreply.github.com>
2024-05-11 20:14:03 +0530

    Refactor livePrediction.py for improved data handling and recent variable initialization

--- livePrediction.py
+++ livePrediction.py
@@ -160,6 +160,7 @@ def csi_data_read_parse():
 
         if startTime + datetime.timedelta(seconds=1) >= datetime.datetime.now():
             count += 1
+            print(startTime)
             print(count)
             timeString = now.strftime("%H:%M:%S.%f")
             csi_list = np.vstack(
